cb6c95c0f6e5a9793d5515759c09352b5e6d0bcb
lizbeth-cortes <lizbethcortes317@aragon.unam.mx>
2023-08-23 20:15:28 -0600

    Lista funcionando

--- musica/src/app/components/lista/lista.component.ts
+++ musica/src/app/components/lista/lista.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CancionService } from 'src/app/services/cancion.service';
+import { Lista } from './lista';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-lista',
@@ -6,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./lista.component.css']
 })
 export class ListaComponent {
-  
+  lista : Lista[] = [];
+  constructor (private cancionService: CancionService, private router: Router){ }
+
+  ngOnInit():void {
+    this.cancionService.getCanciones().subscribe((lista)=>(this.lista = lista));    
+  }
+
+  ver(n:string,artista:string,album:string,anio:string,duracion:string){
+    this.cancionService.setCancion(n,artista,album,parseInt(anio),duracion);
+    this.router.navigate(['detalles'])
+  }
+
+
 }
--- musica/src/app/services/cancion.service.ts
+++ musica/src/app/services/cancion.service.ts
@@ -1,14 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Lista } from '../components/lista/lista';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CancionService {
+  private apiUrl = 'http://localhost:5000/canciones';
   private cancion: String = "";
   private artista: String = "";
   private album: String = "";
   private anio: Number = 0;
   private duracion: String = "";
+
+  constructor(private http : HttpClient){}
+
+  getCanciones() : Observable<Lista[]> {
+    return this.http.get<Lista[]>(this.apiUrl);
+  }
 
   getCancion () {
     return {
@@ -27,6 +38,4 @@ export class CancionService {
     this.anio = an;
     this.duracion = dur;
   }
-
-  constructor() { }
 }
